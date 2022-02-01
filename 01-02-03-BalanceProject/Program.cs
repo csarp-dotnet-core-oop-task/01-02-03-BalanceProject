b1979f7f5ace6d42508e1d05aeff6464fc6c8608
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-01 11:43:37 +0100

    Kész feladat

---
+++ 01-02-03-BalanceProject/Program.cs
@@ -0,0 +1,22 @@
+/*
+A jelenlegi fejlesztő csapata bank számára fejlesztenek szoftvert. Ön a csapat tagjaként, a banki egyenleg feladatrész kifejlesztését kapta. Készítsen egy egyenleg osztályt (Balance) amely két adatot tárol, az egyenleg tulajdonosának a nevét, és az egyenleg összegét. Amikor létrejön egy egyenleg objektum, akkor ezt a két adatot kell megadni. 
+Az egyenleg tulajdonosa pénzt rakhat a számlára és pénzt vehet ki a számláról. Készítse el a Deposit és a Withdraw metódusokat. Negatív pénzösszeget se kivenni sem berakni nem lehet! Többet nem lehet kivenni, mint amennyi a számlán van!
+Készítse el a ToConsole metódusokat amely a következő üzeneteket írhatja ki:
+Pénzes Péter számláján 100 Ft van.
+Pénzes Péter számláján nincsen pénzösszeg.
+Példányosítsa a főprogramban Pénzes Péter számláját 100 Ft-al. Vegyen ki belőle 20 Ft-ot. Rakjon be 50 Ft. Próbáljon berakni -20 Ft-ot. Vegyen ki 1000 Ft.ot! Vegyen ki annyit, hogy a számlán már ne maradjon pénz!. Írja ki minden lépés után a számla egyenlegét!
+Dokumentálja az elkészült osztályt UML osztálydiagrammal!
+*/
+
+using System;
+
+namespace _01_02_03_BalanceProject
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- tests/Tests.cs
+++ tests/Tests.cs
@@ -6,15 +6,202 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace gyak_01_02_01_Maze.Tests
+
+using _01_02_03_BalanceProject;
+
+namespace _01_02_03_BalanceProject.Tests
 {
     [TestClass()]
     public class Tests
     {
         [TestMethod()]
-        public void Test()
+        public void TestKonstrktor()
         {
-            Assert.Fail();
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                expected = "Pénzes Péter számláján 100 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->Konstruktor nem tárolja el a paraméterben lévő adatokat vagy a Balance->ToConsole nem megfelően működik.");
         }
-   }
+
+        [TestMethod()]
+        public void TestKonstrktorNegativAmount()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", -10);
+                expected = "Pénzes Péter számláján nincsen pénzösszeg.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->Konstruktor negatív értékű pénzösszeg esetén helytelenül működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorWithdrawPossible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Withdraw(80);
+                expected = "Pénzes Péter számláján 20 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->Withdraw metódus amikor lehetséges az adott mennyiségű pénz kivétele nem jól működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorWithdrawNotPossible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Withdraw(180);
+                expected = "Pénzes Péter számláján 100 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->Withdraw metódus amikor a megadott pénz mennyiség kivétele nem lehetséges nem jól működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorWithdrawNegativ()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Withdraw(-180);
+                expected = "Pénzes Péter számláján 100 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->Withdraw metódus amikor a negativ a megadott pénz kivétele  nem jól működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorBalanceIsEmpty()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Withdraw(20);
+                balance.Withdraw(80);
+                expected = "Pénzes Péter számláján nincsen pénzösszeg.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A Balance->ToConsole metódus amikor az összes pénzt kivették nem megfelelően működik.");
+        }
+
+
+        [TestMethod()]
+        public void TestKonstrktorBalanceDepositPossible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Deposit(80);
+                expected = "Pénzes Péter számláján 180 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+            Assert.AreEqual(expected, actual, "A Balance->Deposit metódus amikor lehetséges az adott mennyiségű pénz berakása a számlára nem jól működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorBalanceDepositNotPosible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                Balance balance = new Balance("Pénzes Péter", 100);
+                balance.Deposit(-80);
+                expected = "Pénzes Péter számláján 100 Ft van.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                balance.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Balance osztály kivételt dob!");
+            }
+            Assert.AreEqual(expected, actual, "A Balance->Deposit metódus amikor nem lehetséges az adott mennyiségű pénz berakása a számlára nem jól működik vagy a Balance->ToConsole nem megfelően működik.");
+        }
+    }
 }
